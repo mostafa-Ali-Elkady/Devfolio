6c16590f51f55e144f6b3b009bbc0d17190e6457
mostafa-Elkady <mostafaali789@gmail.com>
2023-06-18 00:53:43 +0300

    More Edit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // Start Typing
-  var typed = new Typed(".element", {
-    strings: ["Developer", "Designer", "Freelancer", "Photographer"],
-    typeSpeed: 60,
-    backSpeed: 60,
-    smartBackspace: true,
-    loop: true,
-    loopCount: Infinity,
-  });
+var typed = new Typed(".element", {
+  strings: ["Developer", "Designer", "Freelancer", "Photographer"],
+  typeSpeed: 60,
+  backSpeed: 60,
+  smartBackspace: true,
+  loop: true,
+  loopCount: Infinity,
+});
 // End Typing
 // counter
 const counters = document.querySelectorAll(".count-num"),
@@ -38,3 +38,19 @@ window.addEventListener("scroll", () => {
   }
 })
 // End Counter
+//Start scroll to top button
+$(document).ready(function () {
+  // scroll to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      $(".scroll-top-btn").fadeIn();
+    } else {
+      $(".scroll-top-btn").fadeOut();
+    }
+  });
+
+  $(".scroll-top-btn").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 300);
+  })
+
+});
